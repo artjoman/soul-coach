ce6cdcb2997335668ca26de6407de41a1c3271b1
Artjoms <artjoms.v.petrovs@gmail.com>
2019-11-16 11:08:40 +0200

    Initial general purpose components added. Carousel, sidebar menu, service pges ...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
+import SidebarMenu from './components/sidebar-menu/SidebarMenu';
+import Stream from './pages/stream/Stream';
 
-const App: React.FC = () => {
+function AppRouter() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SidebarMenu>
+      </SidebarMenu>
+      <Router>
+        <div className="container-fluid">
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/stream" component={Stream} />
+        </div>
+      </Router>
+
     </div>
   );
 }
 
-export default App;
+export default AppRouter;
---
+++ frontend/src/pages/stream/Stream.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+ 
+class Stream extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Stream</h2>
+        <p>Carousel 1</p>
+        <p>Carousel 2</p>
+        <p>Carousel 3</p>
+        <p>Carousel 4</p>
+        <p>Carousel 5</p>
+      </div>
+    );
+  }
+}
+ 
+export default Stream;
