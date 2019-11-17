19a639a6601736377880958cd7a59b06d4dc2dbb
Peteris Arajs <peteris.arajs@accenture.com>
2019-11-17 09:25:49 +0200

    Update before relations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
+import Priests from './pages/priests/Priests';
 import Header from './components/header/Header';
 import HeaderWhite from './components/header-white/HeaderWhite';
 import Streams from './pages/stream/Streams';
@@ -16,6 +17,7 @@ function AppRouter() {
         <div className="container-fluid">
           <Route path="/" exact component={Dashboard} />
           <Route path="/streams" component={Streams} />
+          <Route path="/priests" component={Priests} />
         </div>
       </Router>
     </div>
---
+++ frontend/src/pages/priests/Priests.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Priests.css';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, CardImgOverlay
+} from 'reactstrap';
+
+class Priests extends Component {
+    render() {
+        return (
+            <div className="priests-container">
+                    <div className="online-priests">
+                        <Card>
+                            <CardImg className="card-image" src={require('../../assets/images/priest1.png')} alt="Father Samuel" />
+                            <CardTitle>Father Samuel</CardTitle>
+                            <CardSubtitle>Online</CardSubtitle>
+                        </Card>
+                    </div>
+            </div>
+        );
+    }
+}
+
+export default Priests;
